caf101880211035c7fdef15c7a120afaa80dd6bd
zihaowang <zihaowang@ctrip.com>
2019-09-02 17:56:33 +0800

    save

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,11 @@ import * as vscode from 'vscode';
 import EmailFormatterProvider from './provide';
 
 export function activate(context: vscode.ExtensionContext) {
-	const provider = new EmailFormatterProvider();
 
+	//属性替换和emmet有所冲突，需要设置"emmet.showExpandedAbbreviation":"never"
+	vscode.window.showInformationMessage('please set "emmet.showExpandedAbbreviation" to "never" in "settings.json"');
+
+	const provider = new EmailFormatterProvider();
 	const completeProvider = vscode.languages.registerCompletionItemProvider('html',provider,'1','2','3');
 
 	context.subscriptions.push(completeProvider);
--- src/provide.ts
+++ src/provide.ts
@@ -26,7 +26,7 @@ class EmailFormatterProvider implements vscode.CompletionItemProvider {
         const text = document.lineAt(i).text;
         const index = text.indexOf('<td style="font-family:');
         if( index >= 0 && index <= characterNum - 2 ){
-          const size = text.slice(text.indexOf('font-size:') + 10,text.indexOf(';color:'));
+          const size = text.slice(text.indexOf('font-size') + 10,text.indexOf(';color:')-2);
           defaultFontSize = size;
           const color = text.slice(text.indexOf('color') + 5,text.indexOf(';font-family:'));
           defaultColor = color;
